dc7fa3f206dd723349d9a14270c6c544cf364b85
Luis <luisdiazvenero@gmail.com>
2017-08-24 17:23:50 -0500

    initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+	appname: "musicapp",
+	apiKey: "ea8d64603c9dd19cb5540bc0aceab5d5",
+	secret: "9fd436301d9ac703cbd65c105750a3ef",
+	registeredTo: "luisdiazvenero"
+}
+
+// Application name	musicapp
+// API key	ea8d64603c9dd19cb5540bc0aceab5d5
+// Shared secret	9fd436301d9ac703cbd65c105750a3ef
+// Registered to	luisdiazvenero
